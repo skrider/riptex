febb82d43520ed59d7de41406f89192437a84a85
Stephen Krider <skrider@berkeley.edu>
2022-04-21 17:37:50 -0700

    insertion

--- commands/default.go
+++ commands/default.go
@@ -6,19 +6,20 @@ package commands
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"github.com/skrider/riptex/buffer"
 )
 
 type model struct {
-	input  []rune
+	input  buffer.Buffer
 	cursor int
 }
 
 func initialModel() model {
 	return model{
-		input:  []rune("abcdefg"),
+		input:  buffer.NewBuffer(),
 		cursor: 0,
 	}
 }
@@ -31,43 +32,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
-		case tea.KeyEscape, 'q':
+		case tea.KeyEscape:
 			return m, tea.Quit
-		case tea.KeyLeft, 'h':
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case tea.KeyRight, 'l':
-			if m.cursor < len(m.input)-1 {
-				m.cursor++
-			}
-		case tea.KeyBackspace:
-			if m.cursor > 0 {
-				m.input = append(m.input[:(m.cursor-1)], m.input[(m.cursor):]...)
-				m.cursor--
-			}
 		case tea.KeyRunes:
-			input := append(m.input[0:(m.cursor)], msg.Runes...)
-			if m.cursor < len(m.input)-1 {
-				input = append(input, m.input[m.cursor:len(m.input)]...)
-			}
-			m.input = input
-			m.cursor++
+			m.input.Insert(msg.Runes)
 		}
 	}
 	return m, nil
 }
 
-var cursorStyle = lipgloss.NewStyle().Background(lipgloss.Color("#fa695f"))
+// var cursorStyle = lipgloss.NewStyle().Background(lipgloss.Color("#fa695f"))
 
 func (m model) View() string {
-	s := "Type a string: "
-	if m.cursor > 0 {
-		s += string(m.input[:m.cursor])
-	}
-	s += cursorStyle.Render(string(m.input[m.cursor]))
-	s += string(m.input[(m.cursor + 1):])
-	return s
+	var s strings.Builder
+	fmt.Fprint(&s, "Write something: ")
+	fmt.Fprint(&s, m.input.String())
+	return s.String()
 }
 
 func Default() {
